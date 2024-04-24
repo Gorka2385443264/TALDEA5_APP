cd063e01983c612db97cb97deff43cc89b94ecc7
BeñatIturrioz <ben_itu_muj@goierrieskola.net>
2024-04-24 08:57:34 +0200

    aldaketa txikiak

--- src/src/erronka3_talde5/erregistroakLangilea.java
+++ src/src/erronka3_talde5/erregistroakLangilea.java
@@ -74,7 +74,7 @@ public class erregistroakLangilea extends JFrame {
         txtId = new JTextField();
         txtId.setColumns(10);
 
-        JLabel lblId = new JLabel("ID Alokairua:");
+        JLabel lblId = new JLabel("ID LANGILEA:");//a
 
         GroupLayout gl_contentPane = new GroupLayout(contentPane);
         gl_contentPane.setHorizontalGroup(
